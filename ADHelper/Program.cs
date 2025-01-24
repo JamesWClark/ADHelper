d415e8f8a6366f6df204d7a8caf9db4eeed8e9d6
J.W. Clark <this.clark@gmail.com>
2025-01-23 20:20:13 -0800

    Direct test artifacts to TestData directory

--- ADHelper/Program.cs
+++ ADHelper/Program.cs
@@ -5,60 +5,67 @@ using System.DirectoryServices;
 using System.Text.RegularExpressions;
 
 namespace ADHelper {
-	/// <summary>
-	/// 
-	/// </summary>
-	class Program {
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program {
 
-		/**
-		 * @TODO read more, learn to use uname/pword auth
-		 * https://docs.microsoft.com/en-us/dotnet/api/system.directoryservices.accountmanagement?view=dotnet-plat-ext-5.0
-		 */
+        /**
+         * @TODO read more, learn to use uname/pword auth
+         * https://docs.microsoft.com/en-us/dotnet/api/system.directoryservices.accountmanagement?view=dotnet-plat-ext-5.0
+         */
 
-		// ADHelper.exe -csv test.csv -config config.xml -task create_users
-		// ADHelper.exe -csv test.csv -config config.xml -task set_passwords
+        // ADHelper.exe -csv test.csv -config config.xml -task create_users
+        // ADHelper.exe -csv test.csv -config config.xml -task set_passwords
 
 
-		// test data https://docs.google.com/spreadsheets/d/1WjtfjSwoOvFsAnMVRwAyEEczGB7-vM4-ZYbqup4Nv2I/edit#gid=0
+        // test data https://docs.google.com/spreadsheets/d/1WjtfjSwoOvFsAnMVRwAyEEczGB7-vM4-ZYbqup4Nv2I/edit#gid=0
 
-		static void Main(string[] args) {
+        static void Main(string[] args) {
 
-			Config.Options opts = new Config.Options(args);
+            Config.Options opts = new Config.Options(args);
 
-			if(args.Length == 0) {
-				Console.WriteLine("Provide command line arguments, for example: ");
-				Console.WriteLine("1) ADHelper.exe -csv users.csv -config config.xml -task create_users");
-				Console.WriteLine("2) ADHelper.exe -csv users.csv -config config.xml -task set_passwords");
-				Console.ReadLine();
-				return;
+            if(args.Length == 0) {
+                Console.WriteLine("Provide command line arguments, for example: ");
+                Console.WriteLine("1) ADHelper.exe -csv users.csv -config config.xml -task create_users");
+                Console.WriteLine("2) ADHelper.exe -csv users.csv -config config.xml -task set_passwords");
+                Console.ReadLine();
+                return;
             }
-			if(opts.Task.Length == 0) {
-				Console.WriteLine("Task needed.");
-				Console.WriteLine("-task create_user or set_password");
-				return;
-            }
-
-			if(opts.CsvPath.Length == 0) {
-				Console.WriteLine("User data file needed.");
-				return;
+            if(opts.Task.Length == 0) {
+                Console.WriteLine("Task needed.");
+                Console.WriteLine("-task create_user or set_password");
+                return;
             }
 
-			if(opts.ConfigPath.Length == 0) {
-				Console.WriteLine("Config file needed.");
-				return;
+            if(opts.CsvPath.Length == 0) {
+                Console.WriteLine("User data file needed.");
+                return;
             }
 
-			if(opts.Task.Length > 0) {
-				Console.WriteLine("Attempting task with options: ");
-				Console.WriteLine($"Task: {opts.Task}");
-				Console.WriteLine($"Config File: {opts.ConfigPath}");
-				Console.WriteLine($"Domain: {opts.Domain}");
-				Console.WriteLine($"Distinguished Name: {opts.DistinguishedName}");
-				Console.WriteLine($"Input File: {opts.CsvPath}");
-				Console.WriteLine($"- has headers: {opts.InDataHeaders}");
-				Tasks.Task_Batch task = new Tasks.Task_Batch(opts);
-				task.Run();
-			}
-		}
-	}
+            if(opts.ConfigPath.Length == 0) {
+                Console.WriteLine("Config file needed.");
+                return;
+            }
+
+            string outputDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (Environment.GetEnvironmentVariable("RUN_MODE") == "VSCode") {
+                outputDirectory = Path.Combine(outputDirectory, "..", "..", "..", "ADHelper", "TestData");
+            }
+
+            if(opts.Task.Length > 0) {
+                Console.WriteLine("Attempting task with options: ");
+                Console.WriteLine($"Task: {opts.Task}");
+                Console.WriteLine($"Config File: {opts.ConfigPath}");
+                Console.WriteLine($"Domain: {opts.Domain}");
+                Console.WriteLine($"Distinguished Name: {opts.DistinguishedName}");
+                Console.WriteLine($"Input File: {opts.CsvPath}");
+                Console.WriteLine($"- has headers: {opts.InDataHeaders}");
+                Console.WriteLine($"Output Directory: {outputDirectory}");
+                Tasks.Task_Batch task = new Tasks.Task_Batch(opts, outputDirectory);
+                task.Run();
+            }
+        }
+    }
 }
--- ADHelper/Tasks/Task_Batch.cs
+++ ADHelper/Tasks/Task_Batch.cs
@@ -7,195 +7,197 @@ using System.IO;
 using System.Text.RegularExpressions;
 
 namespace ADHelper.Tasks {
-	class Task_Batch {
+    class Task_Batch {
 
-		private List<string> badSamAccountNames = new List<string>();
-		private Config.Options opts;
+        private List<string> badSamAccountNames = new List<string>();
+        private Config.Options opts;
+        private string _outputDirectory;
 
-		public Task_Batch(Config.Options options) {
-			opts = options;
-		}
-
-		public string alphatize(string dirty) {
-			/*
-			string mapDirty = "ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ";
-			string mapClean = "SZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy";
-			string clean = "";
-			bool isCleanCharacter = true;
-			for(int i = 0; i < dirty.Length; i++) {
-				for(int k = 0; k < mapDirty.Length; k++) {
-					if (dirty[i] == mapDirty[k]) {
-						clean += mapClean[k];
-						isCleanCharacter = false;
-						break;
-                    }
-                }
-				if(!isCleanCharacter) {
-					isCleanCharacter = true;
-				} else {
-					clean += dirty[i]; 
-                }
-            }
-			return clean;
-			*/
-			return dirty;
+        public Task_Batch(Config.Options options, string outputDirectory) {
+            opts = options;
+            _outputDirectory = outputDirectory;
         }
 
-		public static string WordListPassword(int n) {
-			string pw = "";
-			Random rand = new Random();
-			for (int i = 0; i < n; i++) {
-				pw += Config.WordList5.Words[rand.Next(0, Config.WordList5.Words.Length)];
-			}
-			String end = "69"; // because high school students
-			while (end == "69") {
-				end = "" + rand.Next(0, 10) + rand.Next(0, 10);
-			}
-			return pw + end;
-		}
+        public string alphatize(string dirty) {
+            /*
+            string mapDirty = "ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ";
+            string mapClean = "SZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy";
+            string clean = "";
+            bool isCleanCharacter = true;
+            for(int i = 0; i < dirty.Length; i++) {
+                for(int k = 0; k < mapDirty.Length; k++) {
+                    if (dirty[i] == mapDirty[k]) {
+                        clean += mapClean[k];
+                        isCleanCharacter = false;
+                        break;
+                    }
+                }
+                if(!isCleanCharacter) {
+                    isCleanCharacter = true;
+                } else {
+                    clean += dirty[i]; 
+                }
+            }
+            return clean;
+            */
+            return dirty;
+        }
 
-		public void Run() {
-			try {
-				//open a users csv and start reading it
-				StreamReader reader = new StreamReader(opts.CsvPath, true);
+        public static string WordListPassword(int n) {
+            string pw = "";
+            Random rand = new Random();
+            for (int i = 0; i < n; i++) {
+                pw += Config.WordList5.Words[rand.Next(0, Config.WordList5.Words.Length)];
+            }
+            String end = "69"; // because high school students
+            while (end == "69") {
+                end = "" + rand.Next(0, 10) + rand.Next(0, 10);
+            }
+            return pw + end;
+        }
 
-				string success_file_path = $"succeeded.{DateTime.Now.ToFileTime()}.csv";
-				string fail_file_path = $"failed.{DateTime.Now.ToFileTime()}.csv";
+        public void Run() {
+            try {
+                //open a users csv and start reading it
+                StreamReader reader = new StreamReader(opts.CsvPath, true);
 
-				int indexImportID = 0,
-					indexFirstName = 1,
-					indexLastName = 2,
-					indexSamAccountName = 3,
-					indexEmail = 4,
-					indexPassword = 5;
+                string success_file_path = Path.Combine(_outputDirectory, $"succeeded.{DateTime.Now.ToFileTime()}.csv");
+                string fail_file_path = Path.Combine(_outputDirectory, $"failed.{DateTime.Now.ToFileTime()}.csv");
 
-				string regexImportID	= Config.Patterns.ImportID;
-				string regexFirstName	= Config.Patterns.FirstName;
-				string regexLastName	= Config.Patterns.LastName;
-				string regexSamAccount	= Config.Patterns.SamAccount;
-				string regexEmail		= Config.Patterns.Email;
-				string regexPassword	= Config.Patterns.Password;
+                int indexImportID = 0,
+                    indexFirstName = 1,
+                    indexLastName = 2,
+                    indexSamAccountName = 3,
+                    indexEmail = 4,
+                    indexPassword = 5;
 
-				string line; // will store each line from the file
+                string regexImportID    = Config.Patterns.ImportID;
+                string regexFirstName   = Config.Patterns.FirstName;
+                string regexLastName    = Config.Patterns.LastName;
+                string regexSamAccount  = Config.Patterns.SamAccount;
+                string regexEmail       = Config.Patterns.Email;
+                string regexPassword    = Config.Patterns.Password;
 
-				//if headers, burn the first line
-				if (opts.InDataHeaders) {
-					line = reader.ReadLine();
-					string[] columns = line.Split(',');
-					
-					for (int i = 0; i < columns.Length; i++) {
-						string header = columns[i].Trim().ToLower();
+                string line; // will store each line from the file
 
-						if (Regex.IsMatch(header, regexImportID)) {
-							indexImportID = i;
-						}
-						if (Regex.IsMatch(header, regexFirstName)) {
-							indexFirstName = i;
-						}
-						if (Regex.IsMatch(header, regexLastName)) {
-							indexLastName = i;
-						}
-						if (Regex.IsMatch(header, regexSamAccount)) {
-							indexSamAccountName = i;
-						}
-						if (Regex.IsMatch(header, regexEmail)) {
-							indexEmail = i;
-						}
-						if (Regex.IsMatch(header, regexPassword)) {
-							indexPassword = i;
-						}
-					}
-				}
-				//read the rest of the file
-				int count = 0;
-				while ((line = reader.ReadLine()) != null) {
+                //if headers, burn the first line
+                if (opts.InDataHeaders) {
+                    line = reader.ReadLine();
+                    string[] columns = line.Split(',');
+                    
+                    for (int i = 0; i < columns.Length; i++) {
+                        string header = columns[i].Trim().ToLower();
 
-					string[] columns = line.Split(',');
+                        if (Regex.IsMatch(header, regexImportID)) {
+                            indexImportID = i;
+                        }
+                        if (Regex.IsMatch(header, regexFirstName)) {
+                            indexFirstName = i;
+                        }
+                        if (Regex.IsMatch(header, regexLastName)) {
+                            indexLastName = i;
+                        }
+                        if (Regex.IsMatch(header, regexSamAccount)) {
+                            indexSamAccountName = i;
+                        }
+                        if (Regex.IsMatch(header, regexEmail)) {
+                            indexEmail = i;
+                        }
+                        if (Regex.IsMatch(header, regexPassword)) {
+                            indexPassword = i;
+                        }
+                    }
+                }
+                //read the rest of the file
+                int count = 0;
+                while ((line = reader.ReadLine()) != null) {
 
-					//try without removing punctuation
-					string importID			= alphatize(columns[indexImportID].Trim());
-					string fname			= alphatize(columns[indexFirstName].Trim());
-					string lname			= alphatize(columns[indexLastName].Trim());
-					string samAccountName	= alphatize(columns[indexSamAccountName].Trim());
-					string email			= alphatize(columns[indexEmail].Trim());
+                    string[] columns = line.Split(',');
 
-					string domain			= email.Split('@')[1];
-					string password = "";
-					if(opts.GeneratePasswords) {
-						password = WordListPassword(2);
+                    //try without removing punctuation
+                    string importID         = alphatize(columns[indexImportID].Trim());
+                    string fname            = alphatize(columns[indexFirstName].Trim());
+                    string lname            = alphatize(columns[indexLastName].Trim());
+                    string samAccountName   = alphatize(columns[indexSamAccountName].Trim());
+                    string email            = alphatize(columns[indexEmail].Trim());
+
+                    string domain           = email.Split('@')[1];
+                    string password = "";
+                    if(opts.GeneratePasswords) {
+                        password = WordListPassword(2);
                     } else {
-						password = columns[indexPassword].Trim();
-					}
+                        password = columns[indexPassword].Trim();
+                    }
 
-					/* let's not do this w/ regex... 
-					// cleaning with regex
-					if (opts.UsernameRegex.Length > 0) {
-						samAccountName = Regex.Replace(samAccountName, opts.UsernameRegex, "");
-						fname = Regex.Replace(fname, opts.UsernameRegex, "");
-						lname = Regex.Replace(lname, opts.UsernameRegex, "");
-					}*/
+                    /* let's not do this w/ regex... 
+                    // cleaning with regex
+                    if (opts.UsernameRegex.Length > 0) {
+                        samAccountName = Regex.Replace(samAccountName, opts.UsernameRegex, "");
+                        fname = Regex.Replace(fname, opts.UsernameRegex, "");
+                        lname = Regex.Replace(lname, opts.UsernameRegex, "");
+                    }*/
 
-					// bad decision? ignores email field from input file
-					// email = samAccountName + opts.Suffix + "@" + domain;
+                    // bad decision? ignores email field from input file
+                    // email = samAccountName + opts.Suffix + "@" + domain;
 
-					try {
-						using (var context = new PrincipalContext(ContextType.Domain, opts.Domain, opts.DistinguishedName)) {
-							// only if setting passwords but not creating accounts
+                    try {
+                        using (var context = new PrincipalContext(ContextType.Domain, opts.Domain, opts.DistinguishedName)) {
+                            // only if setting passwords but not creating accounts
 
-							switch(opts.Task) {
-								case "create_users":
-									using (var user = new UserPrincipal(context)) {
-										// new accounts only
-										user.Name = fname + " " + lname;
-										user.SamAccountName = samAccountName;
-										user.UserPrincipalName = $"{samAccountName}@{opts.Domain}";
-										user.GivenName = fname;
-										user.DisplayName = fname + " " + lname;
-										user.Surname = lname;
-										user.EmailAddress = email;
-										user.SetPassword(password);
-										user.Enabled = true;
-										user.Save();
-										using (var tw = new StreamWriter(success_file_path, true)) {
-											if(count == 0) {
-												tw.WriteLine("Import ID,First Name,Last Name,Email,AD Login,Password");
+                            switch(opts.Task) {
+                                case "create_users":
+                                    using (var user = new UserPrincipal(context)) {
+                                        // new accounts only
+                                        user.Name = fname + " " + lname;
+                                        user.SamAccountName = samAccountName;
+                                        user.UserPrincipalName = $"{samAccountName}@{opts.Domain}";
+                                        user.GivenName = fname;
+                                        user.DisplayName = fname + " " + lname;
+                                        user.Surname = lname;
+                                        user.EmailAddress = email;
+                                        user.SetPassword(password);
+                                        user.Enabled = true;
+                                        user.Save();
+                                        using (var tw = new StreamWriter(success_file_path, true)) {
+                                            if(count == 0) {
+                                                tw.WriteLine("Import ID,First Name,Last Name,Email,AD Login,Password");
                                             }
-											tw.WriteLine($"{importID},{fname},{lname},{email},{samAccountName},{password}");
-											Console.WriteLine("created: " + email);
-										}
-									}
-									break;
-								case "set_passwords":
-									using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, samAccountName)) {
-										user.SetPassword(password);
-										using (var tw = new StreamWriter(success_file_path, true)) {
-											tw.WriteLine($"{fname},{lname},{email},{samAccountName},{password}");
-											Console.WriteLine($"Setting password for record: {fname},{lname},{email},{samAccountName},{password}");
-										}
-									}
-									break;
-								default:
-									// i think this will never throw
-									throw new ArgumentException($"Unsupported Task: {opts.Task}");
+                                            tw.WriteLine($"{importID},{fname},{lname},{email},{samAccountName},{password}");
+                                            Console.WriteLine("created: " + email);
+                                        }
+                                    }
+                                    break;
+                                case "set_passwords":
+                                    using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, samAccountName)) {
+                                        user.SetPassword(password);
+                                        using (var tw = new StreamWriter(success_file_path, true)) {
+                                            tw.WriteLine($"{fname},{lname},{email},{samAccountName},{password}");
+                                            Console.WriteLine($"Setting password for record: {fname},{lname},{email},{samAccountName},{password}");
+                                        }
+                                    }
+                                    break;
+                                default:
+                                    // i think this will never throw
+                                    throw new ArgumentException($"Unsupported Task: {opts.Task}");
                             }
-						}
-					} catch (Exception ex) {
-						Console.WriteLine("failed: " + email);
-						Console.WriteLine(ex.Message);
-						using (var tw = new StreamWriter(fail_file_path, true)) {
-							tw.WriteLine($"{samAccountName},{email},{ex.Message}");
-						}
-						badSamAccountNames.Add(samAccountName);
-					}
-					count++;
-				}
-			} catch (IOException e) {
-				Console.WriteLine(e.Message);
-			}
-			Console.WriteLine("\n\nfails:");
-			foreach (string s in badSamAccountNames) {
-				Console.WriteLine(s);
-			}
-		}
-	}
+                        }
+                    } catch (Exception ex) {
+                        Console.WriteLine("failed: " + email);
+                        Console.WriteLine(ex.Message);
+                        using (var tw = new StreamWriter(fail_file_path, true)) {
+                            tw.WriteLine($"{samAccountName},{email},{ex.Message}");
+                        }
+                        badSamAccountNames.Add(samAccountName);
+                    }
+                    count++;
+                }
+            } catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("\n\nfails:");
+            foreach (string s in badSamAccountNames) {
+                Console.WriteLine(s);
+            }
+        }
+    }
 }
